f909b98985c3ffa962d5c5c73bdc3aa4bac9556c
Mori116 <cacaomame096@gmail.com>
2021-06-17 07:44:17 +0000

    一時コミット

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Item.create!(
-  [
-    {
-      name: 'ケーキ',
-      introduction: '新鮮なイチゴを使用しています。',
-      price: 100,
-      image: File.open('./app/assets/images/img/cake.jpg'),
-      is_active: 0,
-      genre_id: 1,
-    }
-  ]
-)
