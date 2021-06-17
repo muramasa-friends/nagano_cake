e31e94f84d6016d3af671914cf045d9d51561409
maryoryo <liyomaxxde@gmail.com>
2021-06-17 07:44:34 +0000

    [Complete]管理者側顧客一覧

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @admin_customers = Customer.order(created_at: :desc).page(params[:page]).per(10)
+    @admin_customers = Customer.order(created_at: :asc).page(params[:page]).per(10)
   end
 
   def show
