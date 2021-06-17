1b0d890bd281e16de04feb4705768d6a5a6309a6
jizya0208 <moridyan606@gmail.com>
2021-06-17 13:45:06 +0000

    [UPDATE] 配送先一覧

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,10 @@
 class AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.where(customer_id: current_customer)
   end
-
+　
   def edit
+    @address = Address.find(params[:id])
   end
 end
