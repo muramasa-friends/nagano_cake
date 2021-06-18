0ac7b1bb9d11a0d006083da36c1c91ef2a3d0181
jizya0208 <moridyan606@gmail.com>
2021-06-18 08:00:24 +0000

    [ADD] 配送先編集機能

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,8 +3,26 @@ class AddressesController < ApplicationController
     @address = Address.new
     @addresses = Address.where(customer_id: current_customer)
   end
-　
+
+  def create
+    address = Address.new(address_params)
+    address.customer_id = current_customer.id
+    address.save
+    redirect_to addresses_path
+  end
+
   def edit
     @address = Address.find(params[:id])
+  end
+
+  def update
+    address = Address.find(params[:id])
+    address = Address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:customer_id, :postal_code, :address, :name)
   end
 end
