62c174265932fac440fdd9e5fe3f81700b693da7
Mori116 <cacaomame096@gmail.com>
2021-06-18 03:12:46 +0000

    [Add]管理者側の商品情報編集画面を作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,6 +9,22 @@ class Admin::ItemsController < ApplicationController
   def show
   end
 
+
+
   def edit
+    @item = Item.find(params[:id])
+    @items = Item.all
   end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :image, :is_active, :genre_id)
+  end
+
 end
