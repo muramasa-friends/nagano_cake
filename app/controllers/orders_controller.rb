80812b42a519b73a3ae2f2be56aedb9014910cb4
Mori116 <cacaomame096@gmail.com>
2021-06-19 06:41:18 +0000

    一時コミット

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,14 +3,35 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order = Order.new
+    @customer = current_customer
+    @address = Address.new
+  end
+
+  def confirm
+    @cart_item = Customer.cart_item
+    @order = Order.new(order_params)
+  end
+
+  def create
+    @cart_item = Customer.cart_item
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render new
+    end
   end
 
   def show
   end
 
-  def confirm
-  end
-
   def complete
   end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :name, :address, :shipping_fee, :payment_amount, :item_id, :amount, :price)
+  end
+
 end
