a516e6e39e35e5359af93aa0a6efc1e208bdbf14
maryoryo <liyomaxxde@gmail.com>
2021-06-19 03:19:32 +0000

    [Add]管理者側顧客詳細＆編集ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,9 +3,29 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
-    @admin_customer = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.admin_id = current_user.id
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(customer.id)
+    else
+      render 'edit'
+    end
+  end
+  
+  
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
+  end
+
 end
