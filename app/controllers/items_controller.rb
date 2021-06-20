650409eb8528d5d1f2b9123c98744c7c279362d9
maryoryo <liyomaxxde@gmail.com>
2021-06-20 01:34:22 +0000

    [Fix]顧客側商品一覧

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all.order(created_at: :asc).page(params[:page]).per(4)
+    @items = Item.all.order(created_at: :asc).page(params[:page]).per(8)
   end
 
   def show
