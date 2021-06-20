d0c80d4380d1ed0962da8f518785841154df8bb7
maryoryo <liyomaxxde@gmail.com>
2021-06-20 03:13:25 +0000

    [Fix&Add] == trueの追記とページネーションView導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all.order(created_at: :asc).page(params[:page]).per(8)
+    @items = Item.all.order(created_at: :asc).page(params[:page]).per(4)
   end
 
   def show
