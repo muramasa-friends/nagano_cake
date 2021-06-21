cd3b661686c60309216fe46d00c4177a977eebc7
jizya0208 <moridyan606@gmail.com>
2021-06-21 09:57:36 +0000

    [ADD] カート一覧

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,12 +1,12 @@
 class CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.where(customer_id: current_customer)
-    @numbers = Array.new(10){|i| i + 1}
+    @numbers = Array.new(50){|i| i + 1}
   end
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id 
+    @cart_item.customer_id = current_customer.id
     if @cart_item.save
       redirect_to customer_cart_items_path
     else
@@ -16,7 +16,13 @@ class CartItemsController < ApplicationController
     end
   end
 
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to request.referer
+  end
 
+  private
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8822b23a3a55c935e63daeaaa73df1fa3c6853460436043e2966406c8cd312ea7befa1bea177f0c85138a70b91e2b0b3c59d691b19a8ac518fe2373f5f6ed775'
