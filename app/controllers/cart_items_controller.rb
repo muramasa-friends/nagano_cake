f58ca6f8522e0e6dbf97253c249fc9db25e65ac5
maryoryo <liyomaxxde@gmail.com>
2021-06-21 03:57:37 +0000

    [Create]カート追加機能

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,4 +1,33 @@
 class CartItemsController < ApplicationController
+  
   def index
+    
   end
+  
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    if @cart_item.save
+      redirect_to customer_cart_items_path
+    else
+      session[:cart_item] = cart_item_params
+      @item = Item.find(params[:id])
+      render item_path(@item)
+    end
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:item, :amount)
+  end
+  
 end
