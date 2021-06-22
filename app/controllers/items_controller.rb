8af64c84b1516bbb0713bc0108923306251bdeeb
maryoryo <liyomaxxde@gmail.com>
2021-06-22 15:56:25 +0000

    [Fix]ページネーション修正&一覧表示個数修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all.order(created_at: :asc)
-    @item_page = @items.page(params[:page]).per(2)
+    @item_page = @items.page(params[:page]).per(8)
   end
 
   def show
