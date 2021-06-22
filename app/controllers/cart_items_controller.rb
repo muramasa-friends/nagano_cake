a1327d2b475a4c06b7cb7b481bfb899ad73021d7
jizya0208 <moridyan606@gmail.com>
2021-06-22 01:16:10 +0000

    [ADD] 数量変更、削除ボタン

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -21,6 +21,17 @@ class CartItemsController < ApplicationController
     @cart_item.update(cart_item_params)
     redirect_to request.referer
   end
+  
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to request.referer
+  end
+  
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to request.referer
+  end
 
   private
   def cart_item_params
