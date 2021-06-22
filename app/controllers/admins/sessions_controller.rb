6e82ef08648d1c2b34111729da648331f835c2c9
ayk-trsk <1219noppoppo@gmail.com>
2021-06-22 04:27:44 +0000

    [Fix] ログイン機能の修正

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Admins::SessionsController < Devise::SessionsController
-  before_action :reject_customer, only: [:create]
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -19,16 +19,8 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
+  #protected
 
-  def reject_customer
-    @customer = Customer.find_by(email: params[:customer][:email].downcase)
-    if @customer
-      if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
-        redirect_to new_customer_session_path, notice: "You are unsubscribed."
-      end
-    end
-  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Customers::SessionsController < Devise::SessionsController
+    before_action :reject_customer, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +19,17 @@ class Customers::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  
+  def reject_customer
+    @customer = Customer.find_by(email: params[:customer][:email].downcase)
+    if @customer
+      if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
+        redirect_to new_customer_session_path, notice: "You are unsubscribed."
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
