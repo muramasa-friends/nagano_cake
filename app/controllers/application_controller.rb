16f2d919bc2ea7dff3241264d7e182cae38c23df
jizya0208 <moridyan606@gmail.com>
2021-06-23 09:25:12 +0000

    プル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(resource)
-    items_path(resource)
+  
+  def after_inactive_sign_up_path_for(resource)
+    customer_path(current_customer) 
   end
-
+  
   protected
 
   def configure_permitted_parameters
