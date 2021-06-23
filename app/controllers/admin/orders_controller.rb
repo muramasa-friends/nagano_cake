ed32713ac9139d56233eba43b44b36cd98a2f455
maryoryo <liyomaxxde@gmail.com>
2021-06-23 01:16:41 +0000

    [Create]管理者側の商品一覧と注文履歴一覧のページネーション作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @orders = Order.page(params[:page]).reverse_order
+    @orders = Order.page(params[:page]).reverse_order.per(10)
   end
 
   def show
