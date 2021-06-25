8acb0c4d63397bd78508b1f48e110474c5af9227
jizya0208 <moridyan606@gmail.com>
2021-06-25 06:29:23 +0000

    [空こみっと]

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,17 +15,20 @@ class OrdersController < ApplicationController
   def confirm
     @order = Order.new(order_params)
     @cart_items = current_customer.cart_items.all
-
       if params[:order][:address_option] == "0"
         @order.postal_code = current_customer.postal_code
         @order.address = current_customer.address
         @order.name = current_customer.full_name
-
+        
       elsif params[:order][:address_option] == "1"
-       @address = Address.find(params[:order][:address_id])
-       @order.postal_code = @address.postal_code
-       @order.address = @address.address
-       @order.name = @address.name
+        if @address = Address.find(params[:order][:address_id])
+           @order.postal_code = @address.postal_code
+           @order.address = @address.address
+           @order.name = @address.name
+        else
+          render :new
+        end
+
 
       elsif params[:order][:address_option] == "2"
        @order.postal_code = params[:order][:postal_code]
