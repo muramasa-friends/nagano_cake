650aa17847d59e8eabec5bc14c7d0627d6ec3f0f
jizya0208 <moridyan606@gmail.com>
2021-06-25 10:09:05 +0000

    [ADD] トップ遷移時にロゴを表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,22 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+ /*global */
+$(function() {
+	setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
+
+$(document).on('turbolinks:load', function () {
+  setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
