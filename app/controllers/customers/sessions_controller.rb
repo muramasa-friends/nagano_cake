02764e7265449791f74b8de81e7535bcd4196137
ayk-trsk <1219noppoppo@gmail.com>
2021-06-25 19:03:57 +0000

    [Fix]退会処理の修正

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Customers::SessionsController < Devise::SessionsController
-  before_action :reject_customer, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -19,17 +18,8 @@ class Customers::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
+  #protected
 
-
-  def reject_customer
-    @customer = Customer.find_by(email: params[:customer][:email].downcase)
-    if @customer
-      if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
-        redirect_to new_customer_session_path, notice: "You are unsubscribed."
-      end
-    end
-  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,12 +3,4 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true, format: { with: /\p{katakana}/ }
-  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/ }
-  validates :address, presence: true
-  validates :telephone_number, presence: true
-  validates :postal_code, presence: true, length: {is:7}
 end
