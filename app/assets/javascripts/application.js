a0c2a372f11433294ac728811fc332888cc3f13c
maryoryo <liyomaxxde@gmail.com>
2021-06-25 11:31:53 +0000

    [途中]パスワードの表示と非表示アイコン作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,19 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+/*global*/
+
+$(".toggle-password").click(function () {
+    // iconの切り替え
+    $(this).toggleClass("mdi-eye mdi-eye-off");
+
+    // 入力フォームの取得
+    let input = $(this).parent().prev("input");
+    // type切替
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+});
