1c4a83099d24537b72af69a4e6fdee220dce1a91
jizya0208 <moridyan606@gmail.com>
2021-06-27 00:17:31 +0000

    [FIX] リンク

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -83,14 +83,14 @@ window.onload = (function() {
 });
 
 // リンクから遷移した時は非表示
-// $(document).on('turbolinks:load', function () {
-//   setTimeout(function(){
-// 		$('.start p').fadeIn(1600);
-// 	},500); //0.5秒後にロゴをフェードイン!
-// 	setTimeout(function(){
-// 		$('.start').fadeOut(500);
-// 	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
-// });
+$(document).on('turbolinks:load', function () {
+  setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
 
 // TOPページの新着商品を自動スライド
 document.addEventListener("turbolinks:load", function() {
